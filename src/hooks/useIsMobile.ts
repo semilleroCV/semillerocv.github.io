95c4f44109ceb3a8706c91cd1781b36301b1040d
CristianR8 <cristiancamiloreyrueda@gmail.com>
2025-05-13 10:54:48 -0500

    for tablets device

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export function useIsMobile(breakpoint = 768) {
+export function useIsMobile(breakpoint = 1024) {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
