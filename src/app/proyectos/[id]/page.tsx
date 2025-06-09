30a51053fa598cdd958ac55ed34b0ebb041ceb7e
CristianR8 <cristiancamiloreyrueda@gmail.com>
2025-06-09 03:13:14 -0500

    projects section with deploy

--- src/app/proyectos/[id]/page.tsx
+++ src/app/proyectos/[id]/page.tsx
@@ -3,7 +3,11 @@ import { notFound } from "next/navigation";
 import ProjectPage from "@/components/project-page";
 import { projects } from "@/components/projects";
 
-export default function ProjectRoute({ params }) {
+export default function ProjectRoute({
+  params,
+}: {
+  params: { id: string };
+}) {
   const project = projects.find((p) => p.id === params.id);
   if (!project) {
     return notFound();
